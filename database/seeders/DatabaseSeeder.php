17f37628fc36d543fda0cb72a00993c10f2e0a22
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-05-29 19:43:06 -0500

    Seeders and Factorys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Storage;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,7 +12,16 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
+
     {
+        Storage::deleteDirectory('cursos');
+        Storage::makeDirectory('cursos');
         // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(LevelSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(PriceSeeder::class);
+        $this->call(PlatformSeeder::class);
+        $this->call(CourseSeeder::class);
     }
 }
