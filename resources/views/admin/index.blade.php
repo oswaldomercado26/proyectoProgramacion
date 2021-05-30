8c2760b5cb6d138e9a47b3aee752d1136af9d0b3
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-05-29 20:03:22 -0500

    Add plantilla admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', '¡Course')
+
+@section('content_header')
+    <h1>¡Course admin panel.</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
