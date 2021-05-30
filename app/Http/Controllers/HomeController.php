f74fcca873f74cd2d32646fbedba70c1704a7403
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-05-30 15:55:03 -0500

    ventada 1 lista

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Course;
+class HomeController extends Controller
+{
+    public function __invoke(){
+
+        return view('welcome');
+    }
+}
