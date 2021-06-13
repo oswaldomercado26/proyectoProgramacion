a61e9ae021018aa0d332df6bece26b0e2e22bca4
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-06-13 16:44:23 -0500

    vista aprobacion curso

---
+++ resources/views/admin/courses/index.blade.php
@@ -0,0 +1,51 @@
+@extends('adminlte::page')
+
+@section('title', '¡Course')
+
+@section('content_header')
+    <h1>¡Course pendientes de aprobacion.</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Categoria</th>
+                        <th ></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($courses as $course)
+                    <tr>
+                        <td>{{$course->id}}</td>
+                        <td>{{$course->title}}</td>
+                        <td>{{$course->category->name}}</td>
+                        <td>
+                            <a class="btn btn-primary" href="">Revisar</a>
+                        </td>
+                        
+                    </tr>
+                        
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            {{$courses->links('vendor.pagination.bootstrap-4')}}
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CourseController;
 
 Route::get('', [HomeController::class, 'index'])->middleware('can:Ver dashboard')->name('home');
 
@@ -13,3 +14,5 @@ Route::get('', [HomeController::class, 'index'])->middleware('can:Ver dashboard'
 Route::resource('roles', RoleController::class)->names('roles');
 
 Route::resource('users', UserController::class)->only(['index','edit','update'])->names('users');
+
+Route::get('course',[CourseController::class, 'index'])->name('courses.index');
