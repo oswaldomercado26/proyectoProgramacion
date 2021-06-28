02b1ccf25ef96d843765fe094dc9971bc6d5ae9e
Alondra Ramirez <80287792+Ally0199@users.noreply.github.com>
2021-06-28 14:42:00 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <section class="bg-cover" style="background-image: url({{asset('img/home/fondo.jpg')}})">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
-            <h1 class="text-white font-fold text-4xl ">Domina la tegnologia web con ¡Courser</h1>
+            <h1 class="text-white font-fold text-4xl ">Domina la tecnologia web con ¡Courser</h1>
             <p class="text-white text-lg mt-2 mb-4"  > En ¡Course encontraras cursos, manuaes y articulos que te ayudaran a convertirte en un profesional del desarrollo web </p>
             
        @livewire('search')
